62243fee6cd538d2d5f3cba3b885c1143ae51ea0
unknown <singh777komal@gmail.com>
2023-05-31 18:25:13 +0530

    route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import logo from './logo.svg';
 // import './App.css';
 import Login from './pages/SignIn/Login';
+import SignupPage from './pages/Signup/SignUp';
+
 
 function App() {
   return (
    <>
-    <Login/>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login/>}/>
+        <Route path='/signup' element={<SignupPage/>}/>
+      </Routes>
+    </BrowserRouter>
    </>
   );
 }
