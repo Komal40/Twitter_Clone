ae7f6700e2a4c26b448b535bc7a208a87f2f8864
shravanjaju <shravanjaju77@gmail.com>
2023-05-31 23:56:02 +0530

    HomeOk

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 // import './App.css';
 import Login from './pages/SignIn/Login';
 import SignupPage from './pages/Signup/SignUp';
-
+import Sidebar from './Components/Sidebar/Sidebar';
 
 function App() {
   return (
@@ -12,6 +12,8 @@ function App() {
       <Routes>
         <Route path='/' element={<Login/>}/>
         <Route path='/signup' element={<SignupPage/>}/>
+        <Route path='/home' element={<Sidebar/>}/>
+
       </Routes>
     </BrowserRouter>
    </>
